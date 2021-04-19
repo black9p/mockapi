fc9fcd606d41e6bb5dbdd43c058021fe35946ce8
black9p <black9p@naver.com>
2021-04-20 07:21:28 +0900

    first commit

---
+++ src/main/java/me/black9p/mockapi/MockapiApplication.java
@@ -0,0 +1,13 @@
+package me.black9p.mockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/black9p/mockapi/MockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.black9p.mockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
