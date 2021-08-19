51a4b3d290122bfb066808e257a33a3e6544009f
Seongduck Paek <black9p@naver.com>
2021-08-19 22:14:49 +0900

    주석을 추가해본다. (#2)
    * mockapi-1 테스트 이슈
    * mockapi-1 테스트 이슈
    * mockapi-1 테스트 이슈
    * mockapi-1 테스트 이슈
    * mockapi-1 테스트 이슈 주석을 넣어보자
    
    Co-authored-by: baek.sungduk <baek.sungduk@nhnpayco.com>

--- src/main/java/me/black9p/mockapi/MockapiApplication.java
+++ src/main/java/me/black9p/mockapi/MockapiApplication.java
@@ -3,11 +3,12 @@ package me.black9p.mockapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 주석 넣어보기
+ */
 @SpringBootApplication
 public class MockapiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MockapiApplication.class, args);
     }
-
 }
